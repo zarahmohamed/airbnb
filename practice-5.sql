82b8a35062118b7da2148eec30bfb18885c073ff
Zarah Mohamed <zarah.mohamed@kellogg.northwestern.edu>
2023-04-12 22:58:00 +0000

    practice-5_incomplete

--- practice-5.sql
+++ practice-5.sql
@@ -12,4 +12,9 @@
 -- | Private room in townhouse           | 3        |
 -- +-------------------------------------+----------+
 
+SELECT (property_type) 
+FROM listings
+WHERE neighborhood = "Hyde Park"
+GROUP BY property_type
+;
 
